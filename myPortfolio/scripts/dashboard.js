9ed6b0277ad48cf298f5ebf91dd248d623bf87d2
SSchoor163 <steven_schoor@yahoo.com>
2019-04-01 18:01:47 -0500

    Todo list

--- myPortfolio/scripts/dashboard.js
+++ myPortfolio/scripts/dashboard.js
@@ -1,9 +1,27 @@
-$(document).ready(function(){
-   let $section = $('p');
-   let $p = $('p');
-   $p.click(function(){
-        this.innerHTML = "puggggg";
-   }); 
+//preload the website
+$(document).ready(function(){   
+// set welcome to front of page on load
+    display('welcome', 'welBtn');
+
+let $todo = $('#tooDo') // $todo set to todo list container
+let $newTodo = $('#newItemForm');   //$newTodo set to item creation form
+
+// on click event when submit button is pressed. Will take the user input and create  list item with the text and a checkbox below the item creation form
+$newTodo.on('submit', function(e){
+    e.preventDefault();
+    let text = $('input[type="text"]').val(); // set text to the user input
+    let $section = "<section class='oldTodo'><button class='checkbox'></button><p>"+text+"</p></section>"; // the code and the user text to be inserted in the todo list
+    $todo.append($section); // the statement that creates the todo item
+    $('input[type="text"]').val(''); // clear the user input from the item creation field
+});
+
+//on click event. When checkbox is clicked delete todo item
+$todo.on('click', 'button', function(){
+   // point to parent of the checkbox
+    let $this = $(this.parentElement);
+    $this.remove(); //remove that parent
+});
+
 })
 
 var current = 0;
@@ -15,6 +33,8 @@ var current = 0;
             if(current != 0) {document.getElementById(current).style.zIndex="-1";}
             if(curBtn != 0) {document.getElementById(curBtn).style.color="black";}
             current = dis; 
-            curBtn = btn
+            curBtn = btn;
              }
             }
+
+        
